508966ba0b0e91120d2c8d336a892ea47ec4b53c
Edgar Ramirez <edgar-alejandro-fuentes@hotmail.com>
2019-08-18 02:44:01 -0500

    Final Version. I got it

--- scripts/index.js
+++ scripts/index.js
@@ -82,15 +82,62 @@ var app = new Vue({
     },
     computed: {
         checkSolutions(){
-            this.p = (this.a21 * this.a12) - (this.a11 * this.a22);
-            this.q = (this.a21 * this.b1) - (this.a11 * this.b2);
-            this.y = this.q / this.p;
-            this.x = (this.b1 - (this.a12 * this.y))/this.a11;
+            this.p = Number((this.a21 * this.a12) - (this.a11 * this.a22));
+            this.q = Number((this.a21 * this.b1) - (this.a11 * this.b2));
+            this.y = Number(this.q / this.p);
+            this.x = Number((this.b1 - (this.a12 * this.y))/this.a11);
             if(this.p != 0){
-                var punto1Recta1 = (this.b1 / this.a12);
-                var punto2Recta1 = (this.b1 / this.a11);
-                var punto1Recta2 = (this.b2 / this.a22);
-                var punto2Recta2 = ((((Number(this.a21)) * (punto1Recta1 + 20)) * -1) + Number(this.b2)) / Number(this.a22);
+                var pInicio1 = Number((this.b1/this.a12));
+                var pFinalX1 = 0;
+                var pFinalY1 = Number((this.b1/this.a12));
+                console.log( pInicio1+' '+pFinalY1); 
+                var aumentoX1 = Number(this.a12);
+                var aumentoY1 = Number((this.a11) * -1);
+                if(pInicio1 > this.y){
+                    while(pFinalY1 > Number(this.y + aumentoY1)){
+                        pFinalY1 = pFinalY1 + aumentoY1;
+                        pFinalX1 = pFinalX1 + aumentoX1;
+                        console.log( pInicio1+' '+pFinalY1); 
+                    }
+                }else{
+                    var i =0;
+                    while(pFinalY1 <= Number(this.y)){
+                        i++
+                        if(pFinalY1<0){
+                            pFinalY1 = pFinalY1 - aumentoY1;
+                            pFinalX1 = pFinalX1 - aumentoX1;
+                        }else{
+                            pFinalY1 = pFinalY1 + aumentoY1;
+                            pFinalX1 = pFinalX1 + aumentoX1;
+                        }
+                        console.log( pInicio2+' '+pFinalY2); 
+                        if(i>10){
+                            break;
+                        }
+                    }
+                }
+                var pInicio2 = Number((this.b2 / this.a22));
+                var pFinalX2 = 0;
+                var pFinalY2 = Number((this.b2 / this.a22));
+                console.log( 'ff'+pInicio2+' '+pFinalY2);
+                var aumentoX2 = Number(this.a22);
+                var aumentoY2 = Number((this.a21) * -1);
+                if(pInicio2 >= this.y){
+                    
+                    while(pFinalY2 >= (this.y + aumentoY2)){
+                        pFinalY2 = pFinalY2 + aumentoY2;
+                        pFinalX2 = pFinalX2 + aumentoX2;
+                        console.log( pInicio2+' '+pFinalY2); 
+                    }
+                }else{
+                    console.log('Enrté');
+                    while(pFinalY2 <= Number(this.y)){
+                            pFinalY2 = pFinalY2 - aumentoY2;
+                            pFinalX2 = pFinalX2 - aumentoX2;
+                        console.log( pInicio2+' '+pFinalY2); 
+                    }
+                }
+                
                 var chart = new CanvasJS.Chart("chart", {
                     animationEnabled: true,
                     theme: "light2",
@@ -103,16 +150,15 @@ var app = new Vue({
                     data: [{        
                         type: "line",       
                         dataPoints: [
-                            { x: 0, y: punto1Recta1},
-                            { x: punto2Recta1, y: 0}
+                            { x: 0, y: pInicio1},
+                            { x: pFinalX1, y: pFinalY1}
                         ]
                     },
                     {        
                         type: "line",       
                         dataPoints: [
-                            { x: 0, y: punto1Recta2},
-                            // { x: punto2Recta2, y: 0},
-                            { x: (punto1Recta1 + 20), y: punto2Recta2}
+                            { x: 0, y: pInicio2},
+                            { x: pFinalX2, y: pFinalY2}
                         ]
                     },
                     {        
@@ -127,10 +173,19 @@ var app = new Vue({
                 chart.render();
                 return 'Tiene solución única. X= '+ this.x + ' Y= ' + this.y;
             }else if(this.p == 0 && this.q != 0){
-                var punto1Recta1 = (this.b1 / this.a12); //Posición recta 1, primer punto X= 0 y Y= Cálculo.
-                var punto2Recta1 = (this.b1 / this.a11);// Posición recta 1, segundo punto X= Cálculo Y= 0;
-                var punto1Recta2 = (this.b2 / this.a22);//Posición recta 2, primer punto X= 0 y Y= Cálculo.
-                var punto2Recta2 = (this.b2 / this.a21) //Posición recta 2, primer punto X= 0 y Y= Cálculo.
+                var pInicio1 = Number((this.b1/this.a12));
+                console.log(pInicio1);
+                var aumentoX1 = Number(this.a12);
+                var aumentoY1 = Number((this.a11) * -1);
+                var pFinalX1 = 0 + aumentoX1;
+                var pFinalY1 = Number((this.b1/this.a12)) + aumentoY1;
+                console.log( pFinalX1+' '+pFinalY1); 
+                var pInicio2 = Number((this.b2 / this.a22));
+                var aumentoX2 = Number(this.a22);
+                var aumentoY2 = Number((this.a21) * -1);
+                var pFinalX2 = 0 + aumentoX2;
+                var pFinalY2 = Number((this.b2 / this.a22)) + aumentoY2;
+                console.log( pFinalX2+' '+pFinalY2); 
                 var chart = new CanvasJS.Chart("chart", {
                     animationEnabled: true,
                     theme: "light2",
@@ -143,32 +198,41 @@ var app = new Vue({
                     data: [{        
                         type: "line",       
                         dataPoints: [
-                            { x: 0, y: punto1Recta1},
-                            { x: punto2Recta1, y: 0}
+                            { x: 0, y: pInicio1},
+                            { x: pFinalX1, y: pFinalY1}
                         ]
                     },
                     {        
                         type: "line",       
                         dataPoints: [
-                            { x: 0, y: punto1Recta2},
-                            { x: punto2Recta2, y: 0}
+                            { x: 0, y: pInicio2},
+                            { x: pFinalX2, y: pFinalY2}
                         ]
-                    }
+                    },
                 ],
                     
                 });
                 chart.render();
                 return 'No tiene solución.';
             }else if(this.p == 0 && this.q == 0){
-                var punto1Recta1 = (this.b1 / this.a12); //Posición recta 1, primer punto X= 0 y Y= Cálculo.
-                var punto2Recta1 = (this.b1 / this.a11);// Posición recta 1, segundo punto X= Cálculo Y= 0;
-                var punto1Recta2 = (this.b2 / this.a22);//Posición recta 2, primer punto X= 0 y Y= Cálculo.
-                var punto2Recta2 = (this.b2 / this.a21) //Posición recta 2, primer punto X= 0 y Y= Cálculo.
+                var pInicio1 = Number((this.b1/this.a12));
+                var aumentoX1 = Number(this.a12);
+                var aumentoY1 = Number((this.a11) * -1);
+                var pFinalX1 = 0 + aumentoX1;
+                var pFinalY1 = pInicio1 + aumentoY1;
+                console.log( pFinalX1+' '+pFinalY1);  
+                var pInicio2 = Number((this.b2 / this.a22));
+                var aumentoX2 = Number(this.a22);
+                var aumentoY2 = Number((this.a21) * -1);
+                var pFinalX2 = 0 + aumentoX2;
+                var pFinalY2 = pInicio2 + aumentoY2;
+                console.log( pFinalX2+' '+pFinalY2); 
+                
                 var chart = new CanvasJS.Chart("chart", {
                     animationEnabled: true,
                     theme: "light2",
                     title:{
-                        text: "Infinidad de soluciones"
+                        text: "Tiene infinidad de soluciones"
                     },
                     axisY:{
                         includeZero: false
@@ -176,22 +240,22 @@ var app = new Vue({
                     data: [{        
                         type: "line",       
                         dataPoints: [
-                            { x: 0, y: punto1Recta1},
-                            { x: punto2Recta1, y: 0}
+                            { x: 0, y: pInicio1},
+                            { x: pFinalX1, y: pFinalY1}
                         ]
                     },
                     {        
                         type: "line",       
                         dataPoints: [
-                            { x: 0, y: punto1Recta2},
-                            { x: punto2Recta2, y: 0}
+                            { x: 0, y: pInicio2},
+                            { x: pFinalX2, y: pFinalY2}
                         ]
-                    }
+                    },
                 ],
                     
                 });
                 chart.render();
-                return 'Tiene infinidad de soluciones.  Despejado = ' + this.b2 + ' + (' + (this.a21 * -1) + 'x) / ' + this.a22;
+                return 'Tiene infinidad de soluciones';
             }
         },
     },
